ff4b7effe93d8f2675e5e4d6e158b327cdf597c0
Reid Villeneuve <reid.villeneuve@hotmail.com>
2021-10-16 14:54:21 -0600

    edited app.js, forecast.js and geocode.js

---
+++ utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+  const url =
+    'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+    encodeURIComponent(address) +
+    '.json?access_token=pk.eyJ1IjoicmNvZGVyczIwMjEiLCJhIjoiY2t1b3VhaDFlMHh1MzMwbGlibWU3ejB3NyJ9.lkWLNWn0Bq2f9HDHf-M9Wg&limit=1';
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to location services!', undefined);
+    } else if (response.body.features.length === 0) {
+      callback('Unable to find location. Try another search.', undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
+        location: response.body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
